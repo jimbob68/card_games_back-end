13df021c531dbd28e49e19e08e27c1aa29921f6c
James Fraser <jimbob4t@virginmedia.com>
2021-09-30 14:51:49 +0100

    hooked up to the front-end (client)

---
+++ Players.js
@@ -0,0 +1,27 @@
+const players = []
+const addPlayer = ({ id, name, room }) => {
+    name = name.toLowerCase()
+    room = room.toLowerCase()
+    const existingPlayer = players.find(player => player.name === name && player.room === room)
+
+    if(existingPlayer){
+        return {error: "User name is taken"}
+    }
+    const player = {id, name, room}
+    players.push(player)
+
+    return {player}
+}
+
+const removePlayer = (id) => {
+    const index = players.findIndex(player => player.id === id)
+        if(index !== -1){
+            return players.splice(index, 1)[0]
+        }
+        }
+
+const getPlayersInRoom = (room) => {
+    return players.filter(player => player.room === room)
+}
+
+module.exports = {addPlayer, getPlayersInRoom, removePlayer}
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const socketio = require('socket.io')
+const cors = require('cors')
+const http = require('http')
+const PORT = process.env.PORT || 5000
+const router = require("./router")
+const {addPlayer, getPlayersInRoom, removePlayer} = require("./Players.js")
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+app.use(router)
+app.use(cors())
+
+io.on("connection", (socket) => {
+    console.log("new connection:", socket.id)
+    socket.on("join-room", ({ name, room }, callback) => {
+        const {player, error} = addPlayer({id: socket.id, name, room})
+        if(error)return callback(error)
+        socket.join(player.room)
+        io.to(player.room).emit("players-list", {playersList: getPlayersInRoom(player.room)})
+        console.log("getPlayersInRoom:", getPlayersInRoom(player.room))
+        console.log("player.room:", player.room)
+        callback([player])
+    })
+    socket.on("disconnect", () => {
+        console.log("Player has left.")
+        const player = removePlayer(socket.id)
+        if(player){
+            io.to(player.room).emit("players-list", {playersList: getPlayersInRoom(player.room)})
+        }
+    })
+})
+
+
+
+server.listen(PORT, () => console.log("server has started on " + PORT))
+
